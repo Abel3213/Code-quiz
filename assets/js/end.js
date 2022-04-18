bfd040de52faac2c841498fc855ad4c291c9b918
Abel Perez <abel83499@gmail.com>
2022-04-18 15:15:12 -0500

    fixed bugs

--- assets/js/end.js
+++ assets/js/end.js
@@ -20,6 +20,7 @@ saveHighScore = e => {
         score: mostRecentScore,
         name: username.value
     }
+
     highScores.push(score)
 
     highScores.sort((a,b) => {
@@ -29,7 +30,7 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+    window.location.assign('./index.html')
     
 
 }
